f2cf01eb8fe74f8d2b3e6ab78b1e87c81354379f
HotStealer <karl.podra@khk.ee>
2019-01-23 12:31:07 +0200

    Koodi korrastamiseks tõsteti praed htmlist välja

---
+++ menu/menus/menu.php
@@ -0,0 +1,25 @@
+<?php
+$praed = array(
+    array(
+        'nimetus' => 'Sealiha kooreses kastmes',
+        'kirjeldus' => 'sea välisfilee, lisand, kaste, salat, leib',
+        'hind' => 2.65),
+    array(
+        'nimetus' => 'Hakklihapall juustuga',
+        'kirjeldus' => 'hakklihapall 1 tk, lisand, kaste, salat, leib',
+        'hind' => 2.50),
+    array(
+        'nimetus' => 'Pikkpoiss',
+        'kirjeldus' => 'pikkpoiss, lisand, kaste, salat, leib',
+        'hind' => 2.45),
+    array(
+        'nimetus' => 'Ühepajatoit sealihast',
+        'kirjeldus' => 'sealiha köögiviljadega, salat, leib',
+        'hind' => 2,30),
+    array(
+        'nimetus' => 'Pikkpoiss 1/2',
+        'kirjeldus' => 'pikkpoiss, lisand, kaste, salat, leib',
+        'hind' => 1.30)
+);
+
+?>
