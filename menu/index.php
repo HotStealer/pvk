166a391dc88207b010a950f6a2bb2c76622ca7a4
hotstealer <karl.podra@khk.ee>
2019-02-07 13:55:11 +0200

    oop menu - session object

--- menu/index.php
+++ menu/index.php
@@ -1,18 +1,6 @@
 <?php
-require_once 'db/conf.php';
-require_once 'db/db_fnk.php';
-$iktConn = connect_db(DBHOST,DBUSER,DBPASS,DBNAME);
-require_once 'menus/menu.php';
-require_once 'lib/fnk.php';
-pais();
-
-menu(array(
-   'praed' => $praed,
-   'supid' => $supid,
-   'magus' => $magus,
-   'joogid' => $joogid,
-
-));
-
-jalus();
+require_once 'conf.php';
+echo '<pre>';
+print_r($sess);
+echo'</pre>';
 ?>
