9c1e209a056d0cd1b7aefc206d87c85cf862d936
hotstealer <karl.podra@khk.ee>
2019-02-14 09:12:43 +0200

    oop menu - rakenduse peamall täidetud lehe põhistruktuuriga

--- menu/index.php
+++ menu/index.php
@@ -7,7 +7,6 @@ echo'</pre>';
 $mainTmp1= new Template('main');
 
 $mainTmp1->set('title', 'Menu Application');
-$mainTmp1->set('page title', 'Tartu KHK Menu');
 $mainTmp1Content = $mainTmp1->parse();
 
 echo $mainTmp1Content;
